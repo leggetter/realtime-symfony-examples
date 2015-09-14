de94b1a7b6d401f9925d1631826266570f7d10d8
leggetter <phil@leggetter.co.uk>
2015-09-14 16:16:21 +0100

    ratchet app connecting to redis

--- ratchet/src/ChatApp/Chat.php
+++ ratchet/src/ChatApp/Chat.php
@@ -5,40 +5,58 @@ namespace ChatApp;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
+use React\EventLoop\StreamSelectLoop;
+use Predis\Async\Client;
+
 class Chat implements MessageComponentInterface
 {
-    protected $clients;
+    private $clients;
+    
+    private $streamLoop = null;
+    private $redisClient = null;
+    
     public function __construct()
     {
-        $this->clients = new \SplObjectStorage();
+        $this->wsclients = new \SplObjectStorage();
+    
+        $this->streamLoop = new StreamSelectLoop();
+        $this->redisClient = new Client('tcp://127.0.0.1:6379', $this->streamLoop);
+        
+        $this->redisClient->connect(function ($client) {
+          echo "Connected to Redis, now listening for incoming messages...\n";
+
+          $client->pubSubLoop('nrk:channel', function ($event) {
+            echo "Received message `{$event->payload}` from {$event->channel}.\n";
+          });
+          
+        });
 
         echo 'Ratchet Chat server running';
+        
+        $this->streamLoop->run();
     }
+    
     public function onOpen(ConnectionInterface $conn)
     {
         // Store the new connection to send messages to later
-        $this->clients->attach($conn);
+        $this->wsclients->attach($conn);
 
         echo "New connection! ({$conn->resourceId})\n";
     }
+    
     public function onMessage(ConnectionInterface $from, $msg)
     {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s'."\n", $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                $client->send($msg);
-            }
-        }
+        // we don't want to do anything with incoming messages
     }
+    
     public function onClose(ConnectionInterface $conn)
     {
         // The connection is closed, remove it, as we can no longer send it messages
-        $this->clients->detach($conn);
+        $this->wsclients->detach($conn);
 
         echo "Connection {$conn->resourceId} has disconnected\n";
     }
+    
     public function onError(ConnectionInterface $conn, \Exception $e)
     {
         trigger_error("An error has occurred: {$e->getMessage()}\n", E_USER_WARNING);
